2add13bd2477ef0ec5cd4d6bd9453c61a018f393
Ola White <whitesoftx@gmail.com>
2024-03-22 12:59:31 +0100

    new-logo

--- script.js
+++ script.js
@@ -8,15 +8,49 @@ document.addEventListener("DOMContentLoaded", function () {
   const shareBtn = document.getElementById("shareBtn");
   const quoteWrapper = document.getElementById("quoteWrapper");
 
+  // Array of gradient color combinations
+  const gradients = [
+    "linear-gradient(135deg, #800080, #dd1f1f)", // Reddish-purple
+    "linear-gradient(135deg, #008080, #ff8c00)", // Orange to blue-green
+    "linear-gradient(135deg, #ff1493, #00ced1)", // Pink to turquoise
+    "linear-gradient(135deg, #4682b4, #ff6347)", // Steel Blue to Tomato
+    "linear-gradient(135deg, #2e8b57, #ff4500)", // Sea Green to Orange Red
+    "linear-gradient(135deg, #9932cc, #ff7f50)", // Dark Orchid to Coral
+    "linear-gradient(135deg, #1e90ff, #ff69b4)", // Dodger Blue to Hot Pink
+    "linear-gradient(135deg, #8a2be2, #ffd700)", // Blue Violet to Gold
+    "linear-gradient(135deg, #ff0000, #ffff00)", // Red to Yellow
+    "linear-gradient(135deg, #00ff00, #0000ff)", // Green to Blue
+    "linear-gradient(135deg, #ffa500, #8b008b)", // Orange to Dark Magenta
+    "linear-gradient(135deg, #00ffff, #8b4513)", // Cyan to Saddle Brown
+    "linear-gradient(135deg, #ff00ff, #ffdab9)", // Magenta to PeachPuff
+    "linear-gradient(135deg, #6a5acd, #ff4500)", // Slate Blue to Orange Red
+    "linear-gradient(135deg, #008b8b, #b22222)", // Dark Cyan to Fire Brick
+    "linear-gradient(135deg, #ffc0cb, #ff4500)", // Pink to Orange Red
+    "linear-gradient(135deg, #00ced1, #ff69b4)", // Turquoise to Hot Pink
+    "linear-gradient(135deg, #4169e1, #ff4500)", // Royal Blue to Orange Red
+    "linear-gradient(135deg, #2f4f4f, #ff69b4)", // Dark Slate Gray to Hot Pink
+    "linear-gradient(135deg, #d8bfd8, #ff4500)", // Thistle to Orange Red
+  ];
+
   function getRandomProverb() {
     const randomIndex = Math.floor(Math.random() * proverbs.length);
     return proverbs[randomIndex];
   }
 
+  function getRandomGradient() {
+    const randomIndex = Math.floor(Math.random() * gradients.length);
+    return gradients[randomIndex];
+  }
+
   newQuoteBtn.addEventListener("click", function () {
     const { proverb, advice } = getRandomProverb();
+    const randomGradient = getRandomGradient();
+
+    // Apply random gradient color to quote wrapper
+    quoteWrapper.style.background = randomGradient;
+
     quoteText.textContent = proverb;
-    adviceText.innerHTML = "_______________________<br><br>" + advice;
+    adviceText.innerHTML = "____________________<br><br>" + advice;
   });
 
   screenshotBtn.addEventListener("click", function () {
@@ -35,7 +69,6 @@ document.addEventListener("DOMContentLoaded", function () {
           }),
         ],
       };
-
       if (navigator.share) {
         navigator
           .share(shareOptions)
